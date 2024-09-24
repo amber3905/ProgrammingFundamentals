bc29cdf5f92fb3a3f1e5ddb213e7c0a94fb7872f
Betts <ab04209@heron028.eps.surrey.ac.uk>
2024-09-24 15:21:58 +0100

    Java

--- ProblemSets/src/main/java/ProblemSet_1a/HelloWorld.java
+++ ProblemSets/src/main/java/ProblemSet_1a/HelloWorld.java
@@ -1,13 +1,13 @@
 package ProblemSet_1a;
 
 public class HelloWorld {
-	
+
 	public String greeting1;
-	
+
 	public HelloWorld(String greeting) {
 		greeting1 = greeting;
 	}
-	
+
 	public String display() {
 		return greeting1;
 	}
