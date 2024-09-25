690d79b79723add25dc318c73c5072e310815884
Betts <ab04209@duck01.eps.surrey.ac.uk>
2024-09-25 16:10:32 +0100

    Java

--- ProblemSets/src/main/java/ProblemSet_2c/Assessment.java
+++ ProblemSets/src/main/java/ProblemSet_2c/Assessment.java
@@ -5,9 +5,9 @@ public class Assessment {
 	private String name;
 	private double mark;
 
-	public Assessment(String name1, double mark1) {
-		name = name1;
-		mark = mark1;
+	public Assessment(String name, double mark) {
+		this.name = name;
+		this.mark = mark;
 	}
 
 	public String getName() {
--- ProblemSets/src/main/java/ProblemSet_2c/Module.java
+++ ProblemSets/src/main/java/ProblemSet_2c/Module.java
@@ -1,31 +1,18 @@
 package ProblemSet_2c;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Module {
 
-    private String moduleName;
-    private List<Assessment> assessments;
+	private String name;
+	private Assessment assessment1;
+	private Assessment assessment2;
+	private Assessment assessment3;
 
-    public Module(Assessment assessment1, Assessment assessment2, Assessment assessment3) {
-        this.moduleName = "COM1027";
-        assessments = new ArrayList<>();
-        assessments.add(assessment1);
-        assessments.add(assessment2);
-        assessments.add(assessment3);
-    }
+	public double calculateAverage() {
+		double total = assessment1.getMark() + assessment2.getMark() + assessment3.getMark();
+		return total / 3;
+	}
 
-    public double calculateAverage() {
-        double total = 0;
-        for (Assessment assessment : assessments) {
-            total += assessment.getMark();
-        }
-        return total / assessments.size();
-    }
-
-
-    public String toString() {
-        return String.format("%s (%.1f%%)", moduleName, calculateAverage());
-    }
+	public String toString() {
+		return null;
+	}
 }
