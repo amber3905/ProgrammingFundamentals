703fc2ae9851c546b52eccd9b38ed912aa0c8866
Betts <ab04209@duck01.eps.surrey.ac.uk>
2024-09-25 10:50:10 +0100

    Java

---
+++ ProblemSets/src/main/java/ProblemSet_2c/Assessment.java
@@ -0,0 +1,20 @@
+package ProblemSet_2c;
+
+public class Assessment {
+
+	private String name;
+	private double mark;
+
+	public Assessment(String name1, double mark1) {
+		name = name1;
+		mark = mark1;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getMark() {
+		return mark;
+	}
+}
