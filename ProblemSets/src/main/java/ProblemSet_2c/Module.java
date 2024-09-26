d3cc3c3e8784a4cd344c55564079676073c40dd1
Betts <ab04209@duck01.eps.surrey.ac.uk>
2024-09-26 19:47:08 +0100

    Java

--- ProblemSets/src/main/java/ProblemSet_2c/Module.java
+++ ProblemSets/src/main/java/ProblemSet_2c/Module.java
@@ -7,17 +7,17 @@ public class Module {
 	private Assessment assessment2;
 	private Assessment assessment3;
 
-	public Module(Assessment assessment, Assessment assessment4
-			, Assessment assessment5) {
+	public Module(Assessment assessment, Assessment assessment4,
+			Assessment assessment5) {
 		assessment1 = assessment;
 		assessment2 = assessment4;
 		assessment3 = assessment5;
 	}
 
 	public double calculateAverage() {
-		double total = (assessment1.getMark() * 0.2) + 
-				(assessment2.getMark() * 0.2) + 
-				(assessment3.getMark() * 0.6);
+		double total = (assessment1.getMark() * 0.2)
+				+ (assessment2.getMark() * 0.2)
+				+ (assessment3.getMark() * 0.6);
 		return total;
 	}
 
