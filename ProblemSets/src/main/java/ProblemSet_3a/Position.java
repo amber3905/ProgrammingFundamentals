9079bee2cdcf6847a0c70e5ed1b9e6b7b9549e32
Betts <ab04209@duck01.eps.surrey.ac.uk>
2024-09-26 16:14:19 +0100

    Java

---
+++ ProblemSets/src/main/java/ProblemSet_3a/Position.java
@@ -0,0 +1,16 @@
+package ProblemSet_3a;
+
+public class Position {
+
+	private String roleName;
+
+	public void setRoleName(String newRoleName) {
+		roleName = newRoleName;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	
+}
