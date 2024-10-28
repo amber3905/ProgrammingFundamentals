05a6a6927dc58fa70e9d4f511cf733159f0ba9db
Betts <ab04209@duck02.eps.surrey.ac.uk>
2024-10-28 16:38:55 +0000

    Java

---
+++ ProblemSets/src/main/java/ProblemSet_7a/Module.java
@@ -0,0 +1,20 @@
+package ProblemSet_7a;
+
+public class Module {
+
+	private String name;
+
+	public Module(String name) {
+		boolean check = name.matches("(COM)([0-9]{4})");
+		if (check) {
+			this.name = name;
+		} else {
+			this.name = "Error";
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
